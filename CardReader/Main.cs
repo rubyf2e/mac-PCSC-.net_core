6d8b2ce4754c553a1486f7b559d49408ea83bcdf
Ruby <rubyf2e@gmail.com>
2021-05-26 00:49:10 +0800

    git init

---
+++ CardReader/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace CardReader
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
