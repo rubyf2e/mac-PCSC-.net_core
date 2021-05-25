cc14bf45131a65802d7ce07548fccf9fa682b4b5
Ruby <rubyf2e@gmail.com>
2021-05-26 00:50:08 +0800

    mac 可讀取卡片ATR內碼程式

--- CardReader/ViewController.cs
+++ CardReader/ViewController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using AppKit;
 using Foundation;
+using PCSC;
 
 namespace CardReader
 {
+
     public partial class ViewController : NSViewController
     {
         public ViewController(IntPtr handle) : base(handle)
@@ -14,8 +17,112 @@ namespace CardReader
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+            CardstatusLabel.StringValue = "讀卡機無連線";
+            CardstatusLabel.StringValue = PrintReaderState();
 
-            // Do any additional setup after loading the view.
+        }
+
+
+        private string PrintReaderState()
+        {
+            var ReaderStatus   = "目前讀卡機連線機台:";
+            var contextFactory = ContextFactory.Instance;
+            using (var context = contextFactory.Establish(SCardScope.System))
+            {
+                var readerNames = context.GetReaders();
+                foreach (var readerName in readerNames)
+                {
+                    ReaderStatus += "\r" + readerName;
+                }
+            }
+
+            return ReaderStatus;
+        }
+
+
+
+        private string CardStatus()
+        { 
+            var ReaderCardStatus = "";
+            var contextFactory   = ContextFactory.Instance;
+            using (var context = contextFactory.Establish(SCardScope.System)) {
+                var readerNames = context.GetReaders();
+
+                if (IsEmpty(readerNames))
+                {
+                    ReaderCardStatus += "讀卡機未插入";
+                    return ReaderCardStatus;
+                }
+
+                return DisplayReaderStatus(context, readerNames);
+            }
+        }
+
+
+        private string DisplayReaderStatus(ISCardContext context, IEnumerable<string> readerNames)
+        {
+
+            var DisplayReaderStatus = "";
+            foreach (var readerName in readerNames)
+            {
+
+                try
+                {
+                    using (var reader = context.ConnectReader(readerName, SCardShareMode.Shared, SCardProtocol.Any))
+                    {
+                        DisplayReaderStatus += PrintReaderStatus(reader);
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                    return "卡片未插入";
+                }
+            }
+
+            return DisplayReaderStatus;
+
+        }
+
+
+        private string PrintReaderStatus(ICardReader reader)
+        {
+            var PrintReaderStatus = "";
+
+            try
+            {
+                var status = reader.GetStatus();
+                PrintReaderStatus += "序列槽: " + status.Protocol + "\r狀態: " + status.State;
+                PrintReaderStatus += PrintCardAtr(status.GetAtr());
+            }
+            catch (Exception exception)
+            {
+
+                return "\r無法讀取卡片\rError message: " + exception  + exception.GetType();
+            }
+
+            return PrintReaderStatus;
+        }
+
+        private static bool IsEmpty(ICollection<string> readerNames) => readerNames == null || readerNames.Count < 1;
+
+        private string PrintCardAtr(byte[] atr)
+        {
+            if (atr == null || atr.Length <= 0)
+            {
+                return "";
+            }
+
+            return "\r卡片ATR內碼:\r" + BitConverter.ToString(atr) + "\r";
+
+        }
+
+
+
+        partial void readCardClick(Foundation.NSObject sender)
+        {
+            CardstatusLabel.StringValue = "\r\r讀取中...";
+            CardstatusLabel.StringValue = CardStatus();
         }
 
         public override NSObject RepresentedObject
@@ -30,5 +137,10 @@ namespace CardReader
                 // Update the view, if already loaded.
             }
         }
+
+
+
     }
 }
+
+
